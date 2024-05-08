7756f7bcb4fbe6b19719ef7eb0d0254b399cef4d
Abhishek Jaiswar <abhisheknduw@gmail.com>
2024-05-09 02:44:06 +0530

    first commit

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className=''>
+        <div>
+            <h1 className='text-3xl '>Shopping cart</h1>
+        </div>
+        <div>
+            
+        </div>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
